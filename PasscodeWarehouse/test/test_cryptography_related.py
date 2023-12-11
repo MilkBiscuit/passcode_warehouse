bba7b1a81f882e1c6e9bd9e8a7213f0fbadb9327
Chandler Cheng <dc9909@hotmail.com>
2023-12-11 21:54:29 +1300

    Add back 'test_' prefix in method name

--- PasscodeWarehouse/test/test_cryptography_related.py
+++ PasscodeWarehouse/test/test_cryptography_related.py
@@ -4,14 +4,14 @@ from PasscodeWarehouse.domain import cryptography_related
 
 
 class MyTestCase(unittest.TestCase):
-    def given_a_message_when_encrypt_then_decrypt_returns_the_original_message(self):
+    def test_given_a_message_when_encrypt_then_decrypt_returns_the_original_message(self):
         password = 'MyP@ssw0rd'
         cipher_text = cryptography_related.password_encrypt("Hello World", password)
         print("encrypted text is " + cipher_text)
         decoded_message = cryptography_related.password_decrypt(cipher_text, password)
         self.assertEqual("Hello World", decoded_message)
 
-    def given_a_message_and_password_when_encrypt_twice_then_cipher_text_are_different(self):
+    def test_given_a_message_and_password_when_encrypt_twice_then_cipher_text_are_different(self):
         password = '123456'
         message = 'Hello'
         cipher_text_1 = cryptography_related.password_encrypt(message, password)
